2476c6e5a253572484be0a9ff22f66eab754e2c7
Mindless Madness <professor.windu@gmail.com>
2024-02-09 01:32:35 +0530

    shadow acne and lambertian reflection

--- src/print_ppm.cpp
+++ src/print_ppm.cpp
@@ -18,5 +18,6 @@ void print_ppm(std::ostream &out){
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width  = 400;
     cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
     cam.render(world, out); 
 }
