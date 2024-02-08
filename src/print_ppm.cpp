d4e6ccd6a133c1cedc09db5c1ab0c2fb1b83b36d
Mindless Madness <professor.windu@gmail.com>
2024-02-08 21:29:45 +0530

    ray.h

--- src/print_ppm.cpp
+++ src/print_ppm.cpp
@@ -5,8 +5,16 @@
 
 void print_ppm(std::ostream &out){
 
-    int image_width = 256;
-    int image_height = 256;
+    auto aspect_ratio = 16.0/9.0;
+    int image_width = 400;
+    int image_height = static_cast<int>(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 0 : image_height;
+
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
+    
+
     out << "P3\n" << image_width << ' ' << image_height << "\n255\n";
     for(int j=0; j<image_height; ++j){
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
